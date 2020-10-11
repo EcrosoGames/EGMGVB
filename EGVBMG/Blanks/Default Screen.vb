d3f8989b2cb5bc56b37e2a185b082551c2d08e38
Brandon Ecroso <greenbay123456789@gmail.com>
2020-10-10 21:07:11 -0700

    Release 1

---
+++ EGVBMG/Blanks/Default Screen.vb
@@ -0,0 +1,24 @@
+﻿Public Class Default_Screen
+    Inherits BaseScreen
+    Private AniTime As Double = 0
+    Public Sub New()
+        Name = "DefaultScreen"
+        State = ScreenState.Active
+    End Sub
+    Public Overrides Sub HandleInput()
+
+    End Sub
+    Public Overrides Sub Update()
+        'Example of how to control your updates
+        AniTime += Globals.GameTime.ElapsedGameTime.TotalMilliseconds
+        If AniTime > 255 Then
+            AniTime = 0
+
+        End If
+    End Sub
+    Public Overrides Sub Draw()
+        Globals.SpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone)
+
+        Globals.SpriteBatch.End()
+    End Sub
+End Class
